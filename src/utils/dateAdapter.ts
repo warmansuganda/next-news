a4d724c2690292198029c909f6946942ebfd2110
Warman Suganda <warman.suganda@gmail.com>
2022-07-20 17:45:42 +0700

    feat: add datePublished function on date adapter

--- src/utils/dateAdapter.ts
+++ src/utils/dateAdapter.ts
@@ -1,3 +1,26 @@
 import DateAdapter from '@date-io/date-fns';
 
-export default new DateAdapter();
+const adapter = new DateAdapter();
+
+function datePublished(date: Date) {
+  if (date) {
+    const now = new Date();
+    const days = adapter.getDiff(now, date, 'days');
+    const hours = adapter.getDiff(now, date, 'hours');
+    const minutes = adapter.getDiff(now, date, 'minutes');
+
+    if (days)
+      return days <= 10
+        ? `${days}d`
+        : adapter.format(adapter.date(date), 'fullDate');
+    if (hours) return `${hours}h`;
+    if (minutes) return `${minutes}m`;
+  }
+
+  return '-';
+}
+
+export default {
+  ...adapter,
+  datePublished,
+};
